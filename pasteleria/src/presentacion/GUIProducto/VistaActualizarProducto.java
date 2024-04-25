c685cd251c1073fb6dcfaf1800cbabb740a65099
laura <laura@LAPTOP-55L9EQB4>
2024-04-25 18:43:15 +0200

    vista principial producto

--- pasteleria/src/presentacion/GUIProducto/VistaActualizarProducto.java
+++ pasteleria/src/presentacion/GUIProducto/VistaActualizarProducto.java
@@ -1,7 +1,55 @@
 package presentacion.GUIProducto;
 
-import javax.swing.JDialog;
+import java.awt.BorderLayout;
 
-public class VistaActualizarProducto extends JDialog {
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JPanel;
+
+
+import presentacion.Evento;
+import presentacion.IGUI;
+
+public class VistaActualizarProducto extends JDialog implements IGUI {
+
+	public VistaActualizarProducto() {
+		initGUI();
+	}
+	
+	private void initGUI() {
+		setTitle("Actualizar Producto");
+		
+		JPanel mainPanel = new JPanel();
+		mainPanel.setLayout(new BorderLayout());
+		setContentPane(mainPanel);
+		
+		
+		JLabel nombreLabel = new JLabel("Nombre: ");
+		JTextField tFNombre = new JTextField();
+		JPanel nombrePanel = new JPanel();
+		nombrePanel.add(nombreLabel);
+		nombrePanel.add(tFNombre);
+		mainPanel.add(nombrePanel);
+		
+		JLabel stockLabel = new JLabel("Stock: ");
+		JTextField tFStock = new JTextField();
+		JPanel stockPanel = new JPanel();
+		stockPanel.add(stockLabel);
+		stockPanel.add(tFStock);
+		mainPanel.add(stockPanel);
+		
+		JLabel precioLabel = new JLabel("Precio: ");
+		JTextField tFPrecio = new JTextField();
+		JPanel precioPanel = new JPanel();
+		precioPanel.add(precioLabel);
+		precioPanel.add(tFPrecio);
+		mainPanel.add(precioPanel);
+		
+	}
+	
+	@Override
+	public void actualizar(Evento e, Object datos) {
+	}
 
 }
