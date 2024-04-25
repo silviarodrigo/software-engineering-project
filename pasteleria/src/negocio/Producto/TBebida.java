4d785e3c79482b757180c4e279f7d69d477a749b
Daniel <danielbmasia@gmail.com>
2024-04-25 17:49:10 +0200

    producto cosas

---
+++ pasteleria/src/negocio/Producto/TBebida.java
@@ -0,0 +1,10 @@
+package negocio.Producto;
+
+public class TBebida extends TProducto{
+
+	TBebida(String nombre, double precio, String alergenos, int stock, String marca) {
+		super(nombre, precio, alergenos, stock, "Bebida", marca);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ pasteleria/src/presentacion/GUIProducto/VistaListarProducto.java
@@ -0,0 +1,5 @@
+package presentacion.GUIProducto;
+
+public class VistaListarProducto {
+
+}
