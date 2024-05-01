1d19860bae3d9886067f100bdcb28922f87e1401
MiguelG10G <miguelgg998@gmail.com>
2024-05-01 17:35:08 +0200

    TCliente restaurado

--- pasteleria/src/negocio/Cliente/TCliente.java
+++ pasteleria/src/negocio/Cliente/TCliente.java
@@ -1,44 +1,71 @@
 package negocio.Cliente;
 
 public class TCliente {
-	private String nombre_y_apellidos;
-	private int id;
+	private String nombre;
+	private String apellidos;
+	private String dni;
 	private String correo;
 	private boolean activo;
+	private int id;
 	
-	public TCliente (String nom_y_ap, int id, String correo) {
-		this.nombre_y_apellidos = nom_y_ap;
-		this.id = id;
+	
+	public TCliente(String nombre, String apellidos, String dni, String correo) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.dni = dni;
 		this.correo = correo;
 		this.activo = true;
+		this.id = 0;
 	}
 	
+	public TCliente(String nombre, String apellidos, String dni, String correo, boolean activo, int id) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.dni = dni;
+		this.correo = correo;
+		this.activo = activo;
+		this.id = id;
+	}
+	
+	
 	// Getters.
-	public String getNombreYApellidos() {
-		return this.nombre_y_apellidos;
+	public String getNombre() {
+		return this.nombre;
+	}
+
+	public String getApellidos() {
+		return this.apellidos;
+	}
+
+	public String getDNI() {
+		return this.dni;
+	}
+
+	public String getCorreo() {
+		return this.correo;
+	}
+
+	public boolean getActivo() {
+		return this.activo;
 	}
 	
 	public int getId() {
 		return this.id;
 	}
 	
-	public String getCorreo() {
-		return this.correo;
-	}
-	
-	public boolean getActivo() {
-		return this.activo;
-	}
-	
 	// Setters.
-	public void setNombreYApellidos(String nom_y_ap) {
-		this.nombre_y_apellidos = nom_y_ap;
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
 	}
-	
-	public void setId(int id) {
-		this.id = id;
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
 	}
-	
+
+	public void setDNI(String dni) {
+		this.dni = dni;
+	}
+
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
@@ -46,4 +73,9 @@ public class TCliente {
 	public void setActivo(boolean activo) {
 		this.activo=activo;
 	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 }
