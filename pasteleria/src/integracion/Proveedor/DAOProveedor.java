f6506589f5f3226a7eb09ac26196dbd3f938d280
pbermu01 <pbermu01@ucm.es>
2024-05-05 19:57:08 +0200

    añadido DAOProveedor

--- pasteleria/src/integracion/Proveedor/DAOProveedor.java
+++ pasteleria/src/integracion/Proveedor/DAOProveedor.java
@@ -7,7 +7,7 @@ import negocio.Proveedor.TProveedor;
 public interface DAOProveedor {
 
 		public int altaProveedor(TProveedor proveedor);
-		public boolean bajaProveedor(int id);
+		public void bajaProveedor(int id);
 		public int actualizarProveedor(TProveedor proveedor);
 		public TProveedor buscarProveedor(int id); 
 		public TProveedor buscarProveedor(String nombre); 
--- pasteleria/src/integracion/Proveedor/DAOProveedorImp.java
+++ pasteleria/src/integracion/Proveedor/DAOProveedorImp.java
@@ -1,8 +1,16 @@
 package integracion.Proveedor;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.InputStream;
 import java.util.Collection;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import negocio.Proveedor.TProveedor;
 
@@ -11,10 +19,39 @@ public class DAOProveedorImp implements DAOProveedor {
 	@Override
 	public int altaProveedor(TProveedor proveedor) {
 		JSONObject jProv = createJSON(proveedor);
+		JSONObject jO = getJSONFromFile();
+		JSONArray jA;
+		int next_id;
+		
+		//si no tenemos datos preexistentes
+		if (jO == null) {
+			jO = new JSONObject();
+			jA = new JSONArray();
+			next_id = 0;
+			jProv.put("Id", next_id);
+			jA.put(jProv);
+		}
+		//si sí los tenemos
+		else {
+			jA = jO.getJSONArray("Proveedores");
+		    next_id = jO.getInt("next_id");
+		    jProv.put("Id", next_id);
+			jA.put(jProv);
+		}
+		
+		jO.put("Proveedores", jA);
+		jO.put("next_id", next_id+1);
+		
+		if (writeJSONObject(jO)) {
+			return next_id;
+		}
+		else {
+			return -1;
+		}
 	}
 
 	@Override
-	public boolean bajaProveedor(int id) {
+	public void bajaProveedor(int id) {
 		// TODO Auto-generated method stub
 		return false;
 	}
@@ -47,12 +84,35 @@ public class DAOProveedorImp implements DAOProveedor {
 	private JSONObject createJSON(TProveedor proveedor) {
 		JSONObject jObj = new JSONObject();
 		jObj.put("Nombre", proveedor.getNombre());
-		jObj.put("Telefono", proveedor.getCorreo());
+		jObj.put("Telefono", proveedor.getTelefono());
 		jObj.put("Correo", proveedor.getCorreo());
-		jObj.put("Codigp postal", proveedor.getCorreo());
+		jObj.put("Codigo postal", proveedor.getCodigoPostal());
 		jObj.put("Id", proveedor.getID());
 		jObj.put("Activo", proveedor.getActivo());
 		return jObj;
+	}
+	
+	private JSONObject getJSONFromFile() {
+		JSONObject jO;
+		try {
+			InputStream in  = new FileInputStream(new File("resources/Proveedor.json"));
+			jO = new JSONObject(new JSONTokener(in));
+			
+		} catch (FileNotFoundException e) {
+			jO = null;	
+		}
+		return jO;
+	}
+	
+	private boolean writeJSONObject(JSONObject JO) {		
+		try {
+			BufferedWriter bW = new BufferedWriter(new FileWriter("resources/Marca.json"));
+			bW.write(JO.toString(3));
+			bW.close();
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
 	}
 
 }
