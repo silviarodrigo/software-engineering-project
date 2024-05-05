da959ece3c697795734f2dc3a7fe9e211426be5c
pbermu01 <pbermu01@ucm.es>
2024-05-05 19:31:05 +0200

    dao

--- pasteleria/src/integracion/Proveedor/DAOProveedorImp.java
+++ pasteleria/src/integracion/Proveedor/DAOProveedorImp.java
@@ -2,14 +2,15 @@ package integracion.Proveedor;
 
 import java.util.Collection;
 
+import org.json.JSONObject;
+
 import negocio.Proveedor.TProveedor;
 
 public class DAOProveedorImp implements DAOProveedor {
 
 	@Override
 	public int altaProveedor(TProveedor proveedor) {
-		// TODO Auto-generated method stub
-		return 0;
+		JSONObject jProv = createJSON(proveedor);
 	}
 
 	@Override
@@ -40,6 +41,18 @@ public class DAOProveedorImp implements DAOProveedor {
 	public Collection<TProveedor> listarProveedor() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	
+	private JSONObject createJSON(TProveedor proveedor) {
+		JSONObject jObj = new JSONObject();
+		jObj.put("Nombre", proveedor.getNombre());
+		jObj.put("Telefono", proveedor.getCorreo());
+		jObj.put("Correo", proveedor.getCorreo());
+		jObj.put("Codigp postal", proveedor.getCorreo());
+		jObj.put("Id", proveedor.getID());
+		jObj.put("Activo", proveedor.getActivo());
+		return jObj;
 	}
 
 }
