9d76b067fb0f178f4be9d01dfce19c017d642f37
pbermu01 <pbermu01@ucm.es>
2024-05-06 07:08:19 +0200

    Añadida implementación okImp

--- pasteleria/src/presentacion/GUIProveedores/VistaAltaProveedor.java
+++ pasteleria/src/presentacion/GUIProveedores/VistaAltaProveedor.java
@@ -5,6 +5,11 @@ import java.awt.FlowLayout;
 
 import javax.swing.*;
 
+import negocio.Marca.TMarca;
+import negocio.Proveedor.TProveedor;
+import presentacion.Evento;
+import presentacion.Controlador.Controlador;
+
 public class VistaAltaProveedor extends JDialog {
 
 	
@@ -17,15 +22,17 @@ public class VistaAltaProveedor extends JDialog {
 	JTextField _nombreText;
 	JPanel _nombrePanel;
 	
-	JLabel _codigoPostalLabel;
-	JTextField _codigoPostalText;
-	JPanel _codigoPostalPanel;
-	
 	JLabel _telefonoLabel;
 	JTextField _telefonoText;
 	JPanel _telefonoPanel;
 	
+	JLabel _correoLabel;
+	JTextField _correoText;
+	JPanel _correoPanel;
 	
+	JLabel _codigoPostalLabel;
+	JTextField _codigoPostalText;
+	JPanel _codigoPostalPanel;
 	
 	//botones ok y cancel
 	JButton _okButton;
@@ -38,6 +45,8 @@ public class VistaAltaProveedor extends JDialog {
 	
 	//componentes gráficos
 		private void initGUI() {
+			setTitle("Alta Proveedor");
+			
 			
 			JPanel mainVistaAltaPanel = new JPanel();
 			this.setContentPane(mainVistaAltaPanel);
@@ -49,7 +58,7 @@ public class VistaAltaProveedor extends JDialog {
 			//NOMBRE
 			_nombreLabel = new JLabel("Nombre: ");
 			_nombreText = new JTextField();
-			_nombreText.setPreferredSize(new Dimension(50, 25));
+			_nombreText.setPreferredSize(new Dimension(100, 20));
 			_nombrePanel = new JPanel(new FlowLayout());
 			_nombrePanel.add(_nombreLabel);
 			_nombrePanel.add(_nombreText);
@@ -59,18 +68,29 @@ public class VistaAltaProveedor extends JDialog {
 			//TELEFONO
 			_telefonoLabel = new JLabel("Teléfono: ");
 			_telefonoText = new JTextField();
-			_telefonoText.setPreferredSize(new Dimension(50, 25));
+			_telefonoText.setPreferredSize(new Dimension(100, 20));
 			_telefonoPanel = new JPanel(new FlowLayout());
 			_telefonoPanel.add(_telefonoLabel);
 			_telefonoPanel.add(_telefonoText);
 			
 			mainVistaAltaPanel.add(_telefonoPanel);
 			
+			//CORREO
+			_correoLabel = new JLabel("Correo: ");
+			_correoText = new JTextField();
+			_correoText.setPreferredSize(new Dimension(100, 20));
+			_correoPanel = new JPanel(new FlowLayout());
+			_correoPanel.add(_correoLabel);
+			_correoPanel.add(_correoText);
+			
+			mainVistaAltaPanel.add(_correoPanel);
+			
+			
 			//CODIGO POSTAL
 			
 			_codigoPostalLabel = new JLabel("Código Postal: ");
 			_codigoPostalText = new JTextField();
-			_codigoPostalText.setPreferredSize(new Dimension(50, 25));
+			_codigoPostalText.setPreferredSize(new Dimension(100, 20));
 			_codigoPostalPanel = new JPanel(new FlowLayout());
 			_codigoPostalPanel.add(_codigoPostalLabel);
 			_codigoPostalPanel.add(_codigoPostalText);
@@ -88,42 +108,74 @@ public class VistaAltaProveedor extends JDialog {
 			
 			_cancelButton = new JButton("Cancelar");
 			_cancelButton.addActionListener((e)->{
-				cancelImp();	
+				dispose();	
 			});
 			_okCancelPanel.add(_cancelButton);
 			
 			mainVistaAltaPanel.add(_okCancelPanel);
 			
-			
-			
-			
-			//metodos no necesarios pues los ponemos en el open
-			//pack();
-			
-			//setSize(500,500);
-			//setVisible(true);
+			pack();
+			setLocationRelativeTo(null);
+			setVisible(true);
 			
 			
 			
 		}
 		
-		public void open() {
-			pack();
-			this.setLocationRelativeTo(null);
-			setVisible(true);
-		}
 		
 		private void okImp() {
-			SwingUtilities.invokeLater(()->{
-				setVisible(false);
-			});
+			TProveedor proveedor;
+			
+			String nombre = _nombreText.getText();
+			if (nombre == null || nombre.equals("")) {
+				JOptionPane.showMessageDialog(this, "Debe indicar un nombre", "Alta Proveedor", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			
+			int telefono;
+			try {
+				telefono = Integer.parseInt(_telefonoText.getText());
+				if(telefono<0) {
+					JOptionPane.showMessageDialog(this, "El teléfono debe ser positivo", "Alta Proveedor", JOptionPane.ERROR_MESSAGE);
+					return;
+				}else if(_telefonoText.getText().length()!=9) {
+					JOptionPane.showMessageDialog(this, "El teléfono debe tener 9 dígitos", "Alta Proveedor", JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+			}catch (NumberFormatException e) {
+				JOptionPane.showMessageDialog(this, "Teléfono no válido", "Alta Proveedor", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			
+			
+			String correo = _correoText.getText();
+			if (correo == null || correo.equals("")) {
+				JOptionPane.showMessageDialog(this, "Debe indicar un correo", "Alta Proveedor", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			else if (!correo.contains("@") || (!correo.contains(".es") && !correo.contains(".com"))) { 
+				JOptionPane.showMessageDialog(this, "Indique una dirección de correo válida", "Alta Proveedor", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			
+			
+			int codigoPostal;
+			try {
+				codigoPostal = Integer.parseInt(_codigoPostalText.getText());
+				if(codigoPostal<0) {
+					JOptionPane.showMessageDialog(this, "El código postal debe ser positivo", "Alta Proveedor", JOptionPane.ERROR_MESSAGE);
+					return;
+				}
+			}catch (NumberFormatException e) {
+				JOptionPane.showMessageDialog(this, "Código postal no válido", "Alta Proveedor", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+			
+			proveedor = new TProveedor(nombre, telefono, correo, codigoPostal);
+			Controlador.getInstance().accion(Evento.ALTA_PROVEEDOR, proveedor);
+			
 			
 		}
 		
-		private void cancelImp() {
-			SwingUtilities.invokeLater(()->{
-				setVisible(false);
-			});
-		}
 		
 }
