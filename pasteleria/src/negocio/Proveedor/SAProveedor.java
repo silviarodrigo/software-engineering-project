d758db62c65c78f20885a7c42fa3f88c08dde93d
pbermu01 <pbermu01@ucm.es>
2024-05-06 06:19:49 +0200

    Añadidas funciones SAProveedor

--- pasteleria/src/negocio/Proveedor/SAProveedor.java
+++ pasteleria/src/negocio/Proveedor/SAProveedor.java
@@ -1,5 +1,11 @@
 package negocio.Proveedor;
 
-public class SAProveedor {
+import java.util.Collection;
 
+public interface SAProveedor {
+	public int altaProveedor(TProveedor proveedor);
+	public int bajaProveedor(String nombre);
+	public int actualizarProveedor(TProveedor proveedor);
+	public TProveedor buscarProveedor(String nombre);
+	public Collection<TProveedor> listarProveedores();
 }
