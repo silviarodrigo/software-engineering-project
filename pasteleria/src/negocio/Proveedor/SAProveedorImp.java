1d78d3d4dafcbc8e5136b6881bd3dde0d24695d3
pbermu01 <pbermu01@ucm.es>
2024-05-06 06:23:15 +0200

    Empezado

--- pasteleria/src/negocio/Proveedor/SAProveedorImp.java
+++ pasteleria/src/negocio/Proveedor/SAProveedorImp.java
@@ -1,5 +1,37 @@
 package negocio.Proveedor;
 
-public class SAProveedorImp {
+import java.util.Collection;
+
+public class SAProveedorImp implements SAProveedor{
+
+	@Override
+	public int altaProveedor(TProveedor proveedor) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int bajaProveedor(String nombre) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int actualizarProveedor(TProveedor proveedor) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public TProveedor buscarProveedor(String nombre) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<TProveedor> listarProveedores() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
